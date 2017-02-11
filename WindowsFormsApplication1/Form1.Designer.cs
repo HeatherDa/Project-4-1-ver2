844084e6c2644c70c7717024714b378a3fc943d4
HeatherDa <crystal_tree@hotmail.com>
2017-02-10 19:54:15 -0600

    Add project files.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtArea = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPerimeter = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtDepth = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtVolume = new System.Windows.Forms.Label();
+            this.btnAC = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // txtArea
+            // 
+            this.txtArea.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtArea.Location = new System.Drawing.Point(75, 141);
+            this.txtArea.Name = "txtArea";
+            this.txtArea.Size = new System.Drawing.Size(185, 21);
+            this.txtArea.TabIndex = 12;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.AllowDrop = true;
+            this.btnCalculate.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCalculate.Location = new System.Drawing.Point(12, 253);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(89, 25);
+            this.btnCalculate.TabIndex = 7;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "&Length:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "&Width:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtPerimeter
+            // 
+            this.txtPerimeter.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtPerimeter.Location = new System.Drawing.Point(75, 174);
+            this.txtPerimeter.Name = "txtPerimeter";
+            this.txtPerimeter.Size = new System.Drawing.Size(185, 21);
+            this.txtPerimeter.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Area:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 182);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Perimeter:";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(75, 42);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(185, 20);
+            this.txtLength.TabIndex = 2;
+            this.txtLength.TextChanged += new System.EventHandler(this.clearAnswers);
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(75, 77);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(185, 20);
+            this.txtWidth.TabIndex = 4;
+            this.txtWidth.TextChanged += new System.EventHandler(this.clearAnswers);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(185, 253);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtDepth
+            // 
+            this.txtDepth.Location = new System.Drawing.Point(75, 107);
+            this.txtDepth.Name = "txtDepth";
+            this.txtDepth.Size = new System.Drawing.Size(185, 20);
+            this.txtDepth.TabIndex = 6;
+            this.txtDepth.TextChanged += new System.EventHandler(this.clearAnswers);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "&Depth";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(9, 215);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Volume:";
+            // 
+            // txtVolume
+            // 
+            this.txtVolume.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtVolume.Location = new System.Drawing.Point(75, 207);
+            this.txtVolume.Name = "txtVolume";
+            this.txtVolume.Size = new System.Drawing.Size(185, 21);
+            this.txtVolume.TabIndex = 16;
+            // 
+            // btnAC
+            // 
+            this.btnAC.Location = new System.Drawing.Point(118, 253);
+            this.btnAC.Name = "btnAC";
+            this.btnAC.Size = new System.Drawing.Size(46, 23);
+            this.btnAC.TabIndex = 8;
+            this.btnAC.Text = "&AC";
+            this.btnAC.UseVisualStyleBackColor = true;
+            this.btnAC.Click += new System.EventHandler(this.btnAC_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(281, 298);
+            this.Controls.Add(this.btnAC);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtVolume);
+            this.Controls.Add(this.txtDepth);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.txtLength);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtPerimeter);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtArea);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Area, Perimeter, and Volume";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label txtArea;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label txtPerimeter;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtDepth;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label txtVolume;
+        private System.Windows.Forms.Button btnAC;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            calAnswer(); //calculate results
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            populate(); //add question on form load
+        }
+
+        /*the populate function sets text boxes to default setting 0.  
+         * Could have used txtLength.Clear() instead*/
+        private void populate()
+        {
+            txtLength.Text = "0"; 
+            txtWidth.Text = "0";
+            txtDepth.Text = "0";
+            txtArea.Text = "";
+            txtPerimeter.Text = "";
+            txtVolume.Text = "";
+
+            txtLength.Focus();
+        }
+        /* calAnswer is the function that processes input.  
+         * This is where I would do data validation and exception handling
+         */
+        private void calAnswer()
+        {
+            decimal length = Convert.ToDecimal(txtLength.Text); //change Length to decimal
+            decimal width = Convert.ToDecimal(txtWidth.Text);   //change width to decimal
+            decimal depth = Convert.ToDecimal(txtDepth.Text);   //change depth to decimal
+            decimal area;
+            decimal perimeter;
+
+            if(length!=0 && width!= 0){
+                area = length * width;                      //calculate area
+                perimeter = 2 * (length + width);           //calculate perimeter
+                txtArea.Text = Convert.ToString(area);              //display area
+                txtPerimeter.Text = Convert.ToString(perimeter);    //display perimeter
+            }
+            else
+            {
+                txtArea.Text = "Please enter both length and width.";
+                
+            }
+            
+                
+            if (depth !=0){                                     //check if a value for depth has been entered (i.e. object is 3D)
+                decimal volume = length * width * depth;                  //calculate volume for 3D
+                txtVolume.Text = Convert.ToString(volume);      //display volume for 3D
+            }
+            
+            btnAC.Focus();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clearAnswers(object sender, EventArgs e)
+        {
+            txtArea.Text = "";
+            txtPerimeter.Text = "";
+            txtVolume.Text = "";
+        }
+
+        private void btnAC_Click(object sender, EventArgs e)
+        {
+            populate();
+        }
+    }
+}
